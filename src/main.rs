787c44a216c31ec565646a0161bf4b686d6aef58
daniele <felicettadaniele@gmail.com>
2025-05-24 22:16:30 +0200

    feat: some feat

--- src/main.rs
+++ src/main.rs
@@ -7,33 +7,56 @@ struct TrainingData{
 #[derive(Debug)]
 struct PerceptronInputData {
     training_data: TrainingData,
-    learning_curve: f32,
+    init_learning_curve: f32,
     epochs: i32
 }
 
+mod utils;
+use utils::scalar_product;
+use utils::flatVec;
 
 fn perceptron(input:PerceptronInputData){
+    let PerceptronInputData { training_data, epochs,init_learning_curve } = input;
+    let TrainingData { x, y } = training_data;
+
+    let mut learning_curve = 0;
     let mut weights = vec![0,0,0,0];
-    let mut bias = 0;
+    let mut bias:Vec<i32> = Vec::new();
+    
+    let x_flatted = flatVec(x);
+    let mut total_error = 0;
 
-    for epoch in 0..=input.epochs {
-        let mut total_error = 0;
-        let PerceptronInputData { training_data, epochs,learning_curve } = input;
-        let TrainingData { x, y } = training_data;
+    let mut error_sentinel = true;
 
-        for (index,x_i) in x.iter().enumerate() {
-            let y_i = y[index];
+    while (error_sentinel){
+        for epoch in 0..=epochs {
+            for (index,x_i) in x_flatted.iter().enumerate() {
+                let y_i = y[index];
+                let w = weights[index];
+                let b = bias[index];
 
-            let z = weights * x_i + bias;
-             
-        }  
+                let z = w * x_i + b;
+
+                let y_pred = if (z >= 0) {1} else {-1};
+                let error = y_i - y_pred;
+
+                if error != 0 {
+                    weights[index] += learning_curve + error + x_i;
+                    bias[index] += learning_curve + error;
+                    total_error +=1;
+                }
+                else {
+                   error_sentinel = false;
+                }
+            }  
+        }
     }
-
-    println!("Dati X: {:?}",input.training_data.x);
-    println!("Dati Y: {:?}",input.training_data.y);
-    println!("Curva di apprendimento: {}", input.learning_curve);
+    
+    println!("Dati Y: {:?}", y);
+    println!("Curva di apprendimento: {}", input.init_learning_curve);
     println!("Epoche: {}", input.epochs);
 }
+
 
 fn main() {
     let x = vec![[0,1],[0,0],[1,0],[1,1]];
@@ -45,7 +68,7 @@ fn main() {
 
     let percetron_input = PerceptronInputData{
         training_data: training,
-        learning_curve: 0.05,
+        init_learning_curve: 0.05,
         epochs: 10
     };
 
--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,22 @@
-fn scalar_product(x:Vec<i32>, y:Vec<i32>){
+pub fn scalar_product(x:Vec<i32>, y:Vec<i32>) -> Vec<i32>{
+    let mut result = Vec::new();
+    for (index,x_i) in x.iter().enumerate() {
+        let y_i = y[index];
 
+        result[index] = x_i * y_i;
+    }  
+
+    return result;
 }
+
+
+pub fn flatVec(matrix:Vec<[i32;2]>) -> Vec<i32> {
+    let mut flat = Vec::new();
+
+    for row in matrix {
+        for val in row {
+            flat.push(val)
+        }
+    }
+    return flat;
+} 
